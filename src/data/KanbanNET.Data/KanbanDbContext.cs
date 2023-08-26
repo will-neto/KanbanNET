83e0abc8450a935f704e8d8f6d791ea3b1c57f64
Gabriel Fausto <gabriel.fausto@outlook.com>
2023-08-26 20:38:59 -0300

    Inclusão do KanbanDbContext e Projeto API.

---
+++ src/data/KanbanNET.Data/KanbanDbContext.cs
@@ -0,0 +1,25 @@
+﻿using KanbanNET.Services.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.IdentityModel.Tokens;
+
+namespace KanbanNET.Data
+{
+    public class KanbanDbContext : DbContext
+    {
+        public KanbanDbContext(DbContextOptions<KanbanDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Board> Boards { get; set; } = null!;
+        //public DbSet<BoardUsuario> BoardUsuarios { get; set; } = null!;
+        public DbSet<Card> Cards { get; set; } = null!;
+        public DbSet<CardLista> CardListas { get; set; } = null!;
+        public DbSet<Usuario> Usuarios { get; set; } = null!;
+        public DbSet<CardComentario> CardComentarios { get; set; } = null!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(KanbanDbContext).Assembly);
+        }
+    }
+}
---
+++ src/services/KanbanNET.Services/Entities/CardComentario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanbanNET.Services.Entities
+{
+    public class CardComentario
+    {
+        public Guid CardComentarioId { get; private set; }
+        public string Comentario { get; private set; } = null!;
+        public DateTime DataCriacao { get; private set; }
+
+        public Guid CardId { get; private set; }
+        public Guid UsuarioId { get; private set; }
+
+        public Usuario Usuario { get; set; } = null!;
+    }
+}
