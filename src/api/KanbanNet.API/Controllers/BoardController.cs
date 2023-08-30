a9d834342beb4b028ca0ad71ffa6fb272e3e6b19
Gabriel Fausto <gabriel.fausto@outlook.com>
2023-08-30 20:45:59 -0300

    Inclusão do BoardController.

---
+++ src/api/KanbanNet.API/Controllers/BoardController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KanbanNet.API.Controllers
+{
+    [ApiController]
+    [Route("api/boards")]
+    public class BoardController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Adicionar()
+        {
+            return Ok();
+        }
+    }
+}
